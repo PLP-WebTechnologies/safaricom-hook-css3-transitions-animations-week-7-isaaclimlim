b8d92e0c236278459ab0df3ed2403930c731e3f3
isaaclimlim <isaacerukudi@gmail.com>
2025-01-06 12:26:13 +0300

    I have completed assignment

---
+++ script.js
@@ -0,0 +1,34 @@
+// Part 2: Javascript Functions
+
+// Functions with parameters and return values
+
+function calculateRectangleArea(width, height) {
+    return width * height;
+}
+
+const rect = calculateRectangleArea(5,10);
+
+console.log('Rectangle area:', rect);
+
+// Function demonstarting variable scope
+
+let globalVar = 'I am a global variable';
+
+function displayGlobalVar() {
+    let localVar = 'I am a local variable';
+    console.log('Global variable:', globalVar);
+}
+
+displayGlobalVar();
+//console.log('Display global variable:', localVar);
+
+// Function to toggle
+function animateBox() {
+    const box = document.querySelector(".animated-box");
+    box.classList.toggle("show");
+}
+
+const moveBox = document.querySelector("#animate-box-button");
+
+moveBox.addEventListener("click", animateBox);
+
